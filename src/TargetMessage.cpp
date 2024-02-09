9d4dd07c940e6fd65af81d3ee7e0076f1f0da0e7
Igor Sapunov <sapunovigor92@gmail.com>
2024-02-09 12:58:13 +0300

    3

--- src/TargetMessage.cpp
+++ src/TargetMessage.cpp
@@ -4,29 +4,29 @@ void TargetMessage(unsigned char* buf, Target_Message& tm, size_t& index) {
 	//shift index of size data bytes
 	tm.the_Nth_goal_numbering = buf[index + 1];//1
 	++index;
-	tm.vertical_distance = buf[index] + buf[index + 2];//2
+	tm.vertical_distance = buf[index] << 8| buf[index + 2];//2
 	index += 2;
-	tm.lateral_distance = buf[index] + buf[index + 2];//2
+	tm.lateral_distance = buf[index] << 8 | buf[index + 2];//2
 	index += 2;
-	tm.speed_y_direction = buf[index] + buf[index + 2];//2
+	tm.speed_y_direction = buf[index] << 8 | buf[index + 2];//2
 	index += 2;
 	tm.target_type = buf[index];//1
 	++index;
 	tm.lane_number = buf[index];//1
 	++index;
-	tm.front_spacing = buf[index] + buf[index + 2];//2
+	tm.front_spacing = buf[index] << 8 | buf[index + 2];//2
 	index += 2;
-	tm.front_time_interval = buf[index] + buf[index + 2];//2
+	tm.front_time_interval = buf[index] << 8 | buf[index + 2];//2
 	index += 2;
-	tm.speed_x_direction = buf[index] + buf[index + 2];//2
+	tm.speed_x_direction = buf[index] << 8 | buf[index + 2];//2
 	index += 2;
-	tm.heading_angle = buf[index] + buf[index + 2];//2
+	tm.heading_angle = buf[index] << 8 | buf[index + 2];//2
 	index += 2;
 	tm.incidents = buf[index];//1
 	++index;
-	tm.radar_network_X = buf[index] + buf[index + 2] + buf[index + 3] + buf[index + 4];//4
+	tm.radar_network_X = buf[index] << 8 | buf[index + 2] << 8 | buf[index + 3] << 8 | buf[index + 4];//4
 	index += 4;
-	tm.radar_network_Y = buf[index] + buf[index + 2] + buf[index + 3] + buf[index + 4];//4
+	tm.radar_network_Y = buf[index] << 8 | buf[index + 2] << 8 | buf[index + 3] << 8 | buf[index + 4];//4
 	index += 4;
 	tm.fill_the_blind_mark = buf[index];;//1
 	++index;
